a89c4656f192f8d2d59fc68daa6b558fc66322b7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-08-08 00:02:47 +0400

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -7,17 +7,12 @@
 
 /**
  * Generate a random number between the given minimum and maximum, inclusive.
- * @param min: the minimum value
- * @param max: the maximum value
- * @return a random number from [min, max]
  */
 
 Math._random = (min, max) => Math.floor(Math.random() * max) + min;
 
 /**
  * Pad a number with leading 0s up to the given size
- * @param size: size of the string
- * @return the number with leading 0s
  */
 
 // BTDT: don't use an arrow function here
@@ -26,6 +21,5 @@ Number.prototype.zfill = function (size) {
   while (s.length < size) {
     s = '0' + s;
   }
-  
   return s;
 };
