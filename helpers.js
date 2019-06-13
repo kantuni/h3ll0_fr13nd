e8e265afca077be02bcfbc4920aa7c77a1ab7aaa
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-13 14:35:18 -0400

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -8,14 +8,12 @@
 /**
  * Generate a random number between the given minimum and maximum, inclusive.
  */
-
 Math._random = (min, max) => Math.floor(Math.random() * max) + min;
 
 /**
  * Pad a number with leading 0s up to the given size
+ * NOTE: don't use an arrow function here
  */
-
-// BTDT: don't use an arrow function here
 Number.prototype.zfill = function (size) {
   let s = String(this);
   while (s.length < size) {
